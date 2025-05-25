1c7ff7df9c87d879e6e0541a3d847248f3700453
kavinthangavel <kxvinthxngxvel@gmail.com>
2025-05-25 17:10:09 +0530

    build: set version to 2.1.4

--- simkl_mps/__init__.py
+++ simkl_mps/__init__.py
@@ -2,7 +2,7 @@
 Media Player Scrobbler for SIMKL package.
 """
 
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 __author__ = "kavinthangavel"
 
 # Apply compatibility patches first, before any other imports
